4097f2d80cde5e84d60d19ad71280d76d83ce38d
sirlopug <sirlopug@gmail.com>
2019-01-09 23:47:46 -0800

    changed app text

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            <code> ? </code>
+            <code> Sirlopu Technology Group, LLC </code>
           </p>
           <a
             className="App-link"
